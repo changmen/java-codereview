89d986c85353518320725dcc1581c93a02c7df9d
changmen.zx <changmen.zx@taobao.com>
2025-02-27 10:41:40 +0800

    test-add

---
+++ server/src/main/java/com/cr/codereview/model/Item.java
@@ -0,0 +1,58 @@
+package com.cr.codereview.model;
+
+public class Item {
+    private String id;
+    private String name;
+    private String descripton;
+    private String image;
+    private String category;
+    private String skuId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescripton() {
+        return descripton;
+    }
+
+    public void setDescripton(String descripton) {
+        this.descripton = descripton;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSkuId() {
+        return skuId;
+    }
+
+    public void setSkuId(String skuId) {
+        this.skuId = skuId;
+    }
+}
