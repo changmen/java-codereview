840b2c339327172980a0ab1fecc39f15e10ab330
changmen.zx <changmen.zx@taobao.com>
2025-03-04 16:16:53 +0800

    for test

---
+++ server/src/main/java/com/cr/codereview/model/Constants.java
@@ -0,0 +1,6 @@
+package com.example.onlinestore.constants;
+
+public class Constants {
+    public static final String successCode = "success";
+    public static final long successNumCode = 200l;
+}
